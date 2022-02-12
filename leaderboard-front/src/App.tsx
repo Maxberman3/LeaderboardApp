8fa6962e0d607b78682d079e7fd22b9b6e16dba4
Max <max@acumen.io>
2022-02-12 13:56:54 +0200

    create initial react app

--- leaderboard-front/src/App.tsx
+++ leaderboard-front/src/App.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Col, Container, Row } from "react-bootstrap";
+import "./app.css"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Container>
+			<Row>
+				<Col><h1 className='app-header'>Welcome to the Leader Board</h1></Col>
+			</Row>
+			<Row>
+				<Col>
+				</Col>
+			</Row>
+		</Container>
+	);
 }
 
 export default App;
